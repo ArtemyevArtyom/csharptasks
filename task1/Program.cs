bfaf0b4fa2fc930f901bfeeaa15401af441895df
ArtemyevArtyom <gof.ru@yandex.ru>
2022-09-19 21:12:35 +0400

    квадрат второго числа

---
+++ task1/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите число A: ");
+int NumberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число B: ");
+int NumberB = Convert.ToInt32(Console.ReadLine());
+int square = NumberB * NumberB;
+if(square == NumberA)
+{
+    Console.WriteLine("да");
+}
+else
+{
+     Console.WriteLine("нет");
+}
