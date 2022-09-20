b3fef9cf0af567be63fe860116fc1ad6dee373e2
ArtemyevArtyom <gof.ru@yandex.ru>
2022-09-20 15:52:59 +0400

    Программа, которая принимает на вход трёхзначное число и на выходе покахывает последнюю цифру этого числа

---
+++ task4/Program.cs
@@ -0,0 +1,10 @@
+﻿/*
+Напишите программу, которая принимает на вход трёхзначное число и на выходе 
+покахывает последнюю цифру этого числа
+456 -> 6
+*/
+
+Console.Write("Введите трёхзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int ostatok = number % 10;
+Console.WriteLine($"Последняя цифра {ostatok}");
